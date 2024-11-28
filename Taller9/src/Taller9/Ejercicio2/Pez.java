ba2e8d887e4b5cf07baa6756a460783dd52de164
Isac pacheco <isac.pachecolopez@unicolombo.edu.co>
2024-11-27 19:23:35 -0500

    Taller 9

---
+++ Taller9/src/Taller9/Ejercicio2/Pez.java
@@ -0,0 +1,18 @@
+package Taller9.Ejercicio2;
+
+public class Pez extends Animal {
+
+    private String tipoDeAgua;
+
+
+    public Pez(String especie, String tipoDeAgua) {
+        super(especie); 
+        this.tipoDeAgua = tipoDeAgua;
+    }
+
+
+    public void mostrarDetalles() {
+        mostrarEspecie(); 
+        System.out.println("Tipo de Agua: " + tipoDeAgua);
+    }
+}
---
+++ Taller9/src/Taller9/Ejercicio3/Empleado.java
@@ -0,0 +1,10 @@
+package Taller9.Ejercicio3;
+
+class Persona {
+    private String nombre; 
+
+    public Persona(String nombre) {
+        this.nombre = nombre;
+    }
+        
+}
